0179d13e9b5ceb65f2e785f206ca66375f3bc62b
Anton Jardine <156733330+AntonJar-ENU@users.noreply.github.com>
2025-02-13 21:26:48 -0400

    Initial Upload
    Storage Check

---
+++ webStorage/checkStorage.js
@@ -0,0 +1,30 @@
+function storageAvailable(type)
+{
+    try
+    {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+    }
+    catch(e)
+    {
+        return e instanceof DOMException && (
+            e.code === 22 ||
+            e.code === 1014 ||
+            e.name === 'QuotaExceededError' ||
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            storage.length !==0;
+        
+    }
+}
+
+function storageCheck()
+{
+    if(storageAvailable('localStorage'))
+    {
+        alert('Local Storage available');
+    } else {
+        alert('Local Storage not available.')
+    }
+}
