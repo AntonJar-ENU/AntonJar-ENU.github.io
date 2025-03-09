860484b24844e3a525bef72a1085001593f1e2a3
Anton Jardine <156733330+AntonJar-ENU@users.noreply.github.com>
2025-03-09 16:22:02 -0400

    Top Menu Construction

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,4 +1,16 @@
-// Create Array of Menu Links
+// Author: Anton Jardine
+// 
+
+const navLogo = document.querySelector(".euphoria-logo");
+const imgLogo = document.createElement("img");
+imgLogo.src = "images/euphoria-logo.png";
+navLogo.appendChild(imgLogo);
+
+
+// create div for menu items
+const navMenu = document.querySelector(".top-nav-items");
+
+// Create Menu Items in array
 const menuLinks = [
     {text: "Home", link: "home.html"},
     {text: "Events", link: "events.html"},
@@ -9,20 +21,15 @@ const menuLinks = [
     {text: "Contact", link: "contact.html"}
 ]
 
-// Select the Menu List
-const menuItems = document.getElementByIdById("menu-items");
-
-// Add Menu information to the selected Element
+// Extract Menu items from array and place them in the Nav Menu
 menuLinks.forEach(item => {
-
-    let a = document.createElement("a");
     let li = document.createElement("li");
+    let a = document.createElement("a");
 
-    a.href = item.link;
-    li.textContent = item.text
+    a.textContent = item.text;
+    a.href=item.link;
 
     li.appendChild(a);
-    menuLinks.appendChild(li);
+    navMenu.appendChild(li);
 
-
-})
+});
