6d2fa68ea48c3bb6e4f3bcee74d1641b22937520
Anton Jardine <156733330+AntonJar-ENU@users.noreply.github.com>
2025-04-24 11:56:40 -0400

    Updated Comments on JS File, Remove legacy styles and home page

--- js/contact-faq.js
+++ js/contact-faq.js
@@ -6,11 +6,11 @@ var faq = document.getElementsByClassName("faq-question");
 var i;
 for(i=0; i < faq.length; i++){
 
-    // add click event listener to question button and toggle active class to button and parent
+    // add click event listener to question button
     faq[i].addEventListener("click", function () {
-        this.classList.toggle("faq-active");
-        this.parentElement.classList.toggle("faq-active");  // apply 
-
+        this.classList.toggle("faq-active");        //toggle the class for the button to faq-active
+    
+        // show or hide the answer for the corresponding questiong 
         var answer = this.nextElementSibling;
         if(answer.style.display === "block") {
             answer.style.display = "none";
