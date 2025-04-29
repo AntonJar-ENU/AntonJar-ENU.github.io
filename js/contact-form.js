e19822a8e7ef2117f5bfb95f58a09ae620290649
Anton Jardine <156733330+AntonJar-ENU@users.noreply.github.com>
2025-04-29 15:12:35 -0400

    Contact Form Updates to include a field to select question type

--- js/contact-form.js
+++ js/contact-form.js
@@ -5,6 +5,14 @@
     const userName = document.getElementById('name').value.trim();
     const email = document.getElementById('email1').value.trim();
     const confirmEmail = document.getElementById('verify').value.trim();
+    const questionType = document.getElementById('question-type').value;
+
+    // ensure a question type is selected
+    if(questionType === 'none') {
+        alert('Hello ' + userName + '\n Please select a question type.');
+        event.preventDefault();
+        return;
+    }
 
     // check if emails match
     if(email !== confirmEmail) {
