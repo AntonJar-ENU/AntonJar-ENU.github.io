bc2879bf1eadf0c1dad1c14a8d0c847376d47e09
Anton Jardine <156733330+AntonJar-ENU@users.noreply.github.com>
2025-04-28 23:34:28 -0400

    Styling adjustments, Minor bug fixing.

--- js/footer.js
+++ js/footer.js
@@ -2,5 +2,6 @@ const footer = document.querySelector("footer");
 const currentYear = new Date().getFullYear();
 
 const copyright =document.createElement("p");
+copyright.classList.add('footer-text');
 copyright.innerHTML = '&copy' + currentYear + ' Euphoria Latin Dance Academy';
 footer.appendChild(copyright);
